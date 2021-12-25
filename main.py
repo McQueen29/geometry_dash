d39de7650779ed0d7fced66591390c4c87aba9cd
Eugene <sa2lo2880@mail.ru>
2021-12-25 12:28:25 +0300

    to_do_plan

---
+++ main.py
@@ -0,0 +1,65 @@
+import pygame as pg
+
+
+def load_skin():
+    pass
+
+
+def load_level():
+    pass
+
+
+class Walls(pg.sprite.Sprite):
+    pass
+
+
+class DefaultPlayer(pg.sprite.Sprite):
+    pass
+
+
+class SmallPlayer(pg.sprite.Sprite):
+    pass
+
+
+class FlyingPlayer(pg.sprite.Sprite):
+    pass
+
+
+class FlyingPortal(pg.sprite.Sprite):
+    pass
+
+
+class InvertedPortal(pg.sprite.Sprite):
+    pass
+
+
+class SmallPortal(pg.sprite.Sprite):
+    pass
+
+
+class Jumper(pg.sprite.Sprite):
+    pass
+
+
+class InverterPoint(pg.sprite.Sprite):
+    pass
+
+
+class JumpPoint(pg.sprite.Sprite):
+    pass
+
+
+class Coin(pg.sprite.Sprite):
+    pass
+
+
+class Statistics():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
